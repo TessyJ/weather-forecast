9987b7b1ee9ff9169cf24519ea0119e875f60c6f
Theresa <theresaolusoga@Bolajis-MacBook-Pro.local>
2023-03-18 11:13:53 -0400

    declared all functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,3 +25,49 @@ function searchHandler(event) {
 }
 
 searchBtnEl.addEventListener('click',searchHandler)
+
+//function getWeather
+
+async function getWeather(city){
+
+}
+
+//function getCurrendate
+
+function getCurrentDate(){
+
+}
+
+// Displays the current weather data for a city 
+function displayWeather(data) {
+
+}
+
+//Retrieves the 5-day forecast data for a city
+function getForecast(coords){
+
+}
+
+// Saves a city to local storage
+function saveCity(city){
+
+}
+
+//load saved cities
+function loadCities() {
+
+}
+
+//display   search  history
+function displaySearchHistory() {
+
+}
+
+//Displays the 5-day forecast data for a city
+
+function displayForecast(data){
+
+}
+
+function historyHandler(event) {
+}
